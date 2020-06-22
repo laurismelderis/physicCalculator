cda4d8685bc8e67408c867bf4330b948c5682fea
laurispc <laurismelderis>
2020-06-22 17:11:28 +0300

    Initial commit

---
+++ src/physicscalculator/PhysicsCalculator.java
@@ -0,0 +1,8 @@
+package physicscalculator;
+
+public class PhysicsCalculator{
+    public static void main(String[] args) {
+        new MainFrame().setVisible(true);
+    }
+    
+}
---
+++ src/physicscalculator/TextFileDb.java
@@ -0,0 +1,18 @@
+package physicscalculator;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class TextFileDb {
+    public TextFileDb(){
+        
+    }
+    public void createFile(){
+        
+    }
+    public void openFile(){
+        
+    }
+}
